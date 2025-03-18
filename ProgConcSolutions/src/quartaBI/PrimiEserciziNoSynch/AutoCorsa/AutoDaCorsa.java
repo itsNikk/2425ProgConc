1e780b7cea9d96719735e14e39072c42a8024814
iTzNikk <nicolo.buganza@tiscali.it>
2025-03-18 21:53:41 +0100

    Aggiunti primi esercizi4BI

---
+++ ProgConcSolutions/src/quartaBI/PrimiEserciziNoSynch/AutoCorsa/AutoDaCorsa.java
@@ -0,0 +1,42 @@
+package quartaBI.PrimiEserciziNoSynch.AutoCorsa;
+
+public class AutoDaCorsa implements Runnable {
+    private String nome;
+    private int velocita = 0; // Velocità iniziale
+    private static final int PIT_STOP_INTERVAL = 5; // Ogni 5 iterazioni
+    private static final int PIT_STOP_DURATA = 2000; // 2 secondi
+
+    public AutoDaCorsa(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void run() {
+        int iterazioni = 0;
+        while (true) {
+            velocita += 20;
+            iterazioni++;
+
+            System.out.println(nome + " - Velocità attuale: " + velocita + " km/h");
+
+            // Pit stop ogni 5 iterazioni
+            if (iterazioni % PIT_STOP_INTERVAL == 0) {
+                System.out.println(nome + " entra ai box per un pit stop! 🏁");
+                try {
+                    Thread.sleep(PIT_STOP_DURATA); // Simula il pit stop
+                } catch (InterruptedException e) {
+                    System.out.println(nome + " ha terminato la gara!");
+                    break;
+                }
+            }
+
+            // Pausa ed eventuale terminazione
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(nome + " ha terminato la gara!");
+                break;
+            }
+        }
+    }
+}
---
+++ ProgConcSolutions/src/quartaBI/PrimiEserciziNoSynch/MatrixEffect/Main.java
@@ -0,0 +1,7 @@
+package quartaBI.PrimiEserciziNoSynch.MatrixEffect;
+
+public class Main {
+    public static void main(String[] args) {
+            new MatrixEffect().start();
+    }
+}
