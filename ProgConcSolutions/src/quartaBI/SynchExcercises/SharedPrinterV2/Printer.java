af19d35b5e8f276d827f7b286d7cd86a3e43cd01
Nicolò Buganza <nicolo.buganza@tiscali.it>
2025-04-15 13:29:47 +0200

    Added SharedPrinterV2 4bi

---
+++ ProgConcSolutions/src/quartaBI/SynchExcercises/SharedPrinterV2/Printer.java
@@ -0,0 +1,34 @@
+package quartaBI.SynchExcercises.SharedPrinterV2;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+//Di SOLITO, le risorse condivise NON sono thread
+public class Printer {
+
+    private Semaphore semaphore;
+    private int docsInQueue;
+    private int docsPrinted;
+
+    private Random r;
+
+    public Printer() {
+        this.semaphore = new Semaphore(1);
+        this.r = new Random();
+    }
+
+    public void print(Document doc) {
+        System.out.println(Thread.currentThread().getName() + " cerca di stampare..." + doc.getTitle() + "...");
+        try {
+            semaphore.acquire();
+            System.out.println(Thread.currentThread().getName() + " inizia la stampa di " + doc.getTitle() + "...");
+            Thread.sleep(r.nextInt(500, 1500));
+            docsPrinted++;
+            System.out.println(Thread.currentThread().getName() + " finisce la stampa di " + doc.getTitle() + "...");
+        } catch (InterruptedException e) {
+            System.out.println(Thread.currentThread().getName() + " interrotto.");
+        }
+        //PERCHE' è meglio qui?
+        semaphore.release();
+    }
+}
---
+++ ProgConcSolutions/src/quartaBI/SynchExcercises/SharedPrinterV2/PrinterThread.java
@@ -0,0 +1,31 @@
+package quartaBI.SynchExcercises.SharedPrinterV2;
+
+import javax.print.Doc;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PrinterThread extends Thread {
+
+    private Printer printer;
+    private ArrayList<Document> docs;
+
+    public PrinterThread(Printer p) {
+        this.printer = p;
+        this.docs = generateDocs();
+
+    }
+
+    private ArrayList<Document> generateDocs() {
+        ArrayList<Document> results = new ArrayList<>();
+        int maxDocs = new Random().nextInt(2, 4);
+        for (int i = 0; i < maxDocs; i++) {
+            results.add(new Document("DOCTITLE:" + (i + 1), "Il testo del doc" + (i + 1)));
+        }
+        return results;
+    }
+
+    @Override
+    public void run() {
+        for (Document doc : docs) printer.print(doc);
+    }
+}
