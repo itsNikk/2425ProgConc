a0dac5e19c1d15cc58730382d551a9cf9b30c170
Nicolò Buganza <nicolo.buganza@tiscali.it>
2025-04-29 13:34:24 +0200

    Update ParkingLot.java

--- ProgConcSolutions/src/quartaBI/SynchExcercises/ParkingLotSynchronized/ParkingLot.java
+++ ProgConcSolutions/src/quartaBI/SynchExcercises/ParkingLotSynchronized/ParkingLot.java
@@ -7,14 +7,16 @@ public class ParkingLot {
         this.postiLiberi = postiLiberiIniziali;
     }
 
-    public synchronized void enter(Car c) {
-        System.out.println(c.getName() + " entra nel parcheggio (posti liberi: " + postiLiberi + ")");
+    public synchronized void enter(Car c) throws InterruptedException {
+        if (postiLiberi <= 0) wait();
         postiLiberi--;
+        System.out.println(c.getName() + " entra nel parcheggio (posti liberi: " + postiLiberi + ")");
     }
 
     public synchronized void exit(Car c) {
         System.out.println(c.getName() + " esce dal parcheggio (PL: " + postiLiberi + ")");
         postiLiberi++;
+        notify();
     }
 
 }
